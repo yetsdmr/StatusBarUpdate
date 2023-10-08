8c494465059fd7b5e20bd326ed0cbd4dc0c30212
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-10-08 12:01:02 +0300

    Initial Commit

---
+++ StatusBarUpdate/StatusBarUpdateApp.swift
@@ -0,0 +1,17 @@
+//
+//  StatusBarUpdateApp.swift
+//  StatusBarUpdate
+//
+//  Created by Yunus Emre Taşdemir on 8.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct StatusBarUpdateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
